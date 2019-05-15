084a2b723bdf125a973f98ec05039e3b583fa353
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2019-05-15 10:14:58 +0530

    Update 66.py

--- 66.py
+++ 66.py
@@ -2,9 +2,9 @@ num1,num2,num3=input().split()
 n1=int(num1)
 n2=int(num2)
 n3=int(num3)
-if(num1>num2) and (num1>num3):
-    print(num1)
-elif(num2>num1) and (num2>num3):
-    print(num2)
+if(n1>n2) and (n1>n3):
+    print(n1)
+elif(n2>n1) and (n2>n3):
+    print(n2)
 else:
-    print(num3)
+    print(n3)
