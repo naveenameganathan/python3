fc25522ba4097369e071aba98995b53114fad5f9
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2019-05-15 09:14:37 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,5 +1,5 @@
 wow=input()
 if(wow=='a' or wow=='e' or wow=='i' or wow=='o' or wow=='u' or wow=='A' or wow=='E' or wow=='I' or wow=='O' or wow=='U'):
-	       print("Vowel")
+       print("Vowel")
 else:
-	       print("Consonent")
+       print("Consonent")
