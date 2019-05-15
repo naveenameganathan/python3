f08781464d63a07f6bd1916ae56fe37844b66b7f
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2019-05-15 09:09:33 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,4 +1,4 @@
-wow=input("enter your character")
+wow=input()
 if(wow=='a' or wow=='e' or wow=='i' or wow=='o' or wow=='u' or wow=='A' or wow=='E' or wow=='I' or wow=='O' or wow=='U'):
 	       print("Vowel",wow)
 else:
