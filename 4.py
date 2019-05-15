ea05a963dc8732c939279b973b43fbdea843056b
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2019-05-15 09:06:37 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,5 @@
+wow=input("enter your character")
+if(wow=='a' or wow=='e' or wow=='i' or wow=='o' or wow=='u' or wow=='A' or wow=='E' or wow=='I' or wow=='O' or wow=='U'):
+	       print("Vowel",wow)
+else:
+	       print("Consonent",wow)
