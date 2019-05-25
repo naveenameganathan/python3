e4408114abe466650f25374517a31a1c3732b289
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2019-05-25 23:36:33 +0530

    Update 16dispprime.py

--- 16dispprime.py
+++ 16dispprime.py
@@ -1,9 +1,9 @@
-start=int(input())
-end=int(input())
-for num in range(start,end+1):
-    if(num>1):
-        for i in range(2,num):
-            if(num % i == 0):
+a=int(input())
+b=int(input())
+for i in range(a,b+1):
+    if(i>1):
+        for j in range(2,i):
+            if(i % j == 0):
                 break
         else:
-             print(num)
+                print(i,end=" ")   
