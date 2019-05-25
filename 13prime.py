ae21cb4a8643214cced407365574559b8296d256
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2019-05-25 14:33:13 +0530

    Update 13prime.py

--- 13prime.py
+++ 13prime.py
@@ -1,7 +1,9 @@
-num=13
+num=int(input())
 if num>1:
     for i in range(2,num):
         if(num % i)==0:
             print("no")
-        else:
-            print("yes")
+            break
+    else:
+        print("yes")
+    
