2367b2826f8ffe9badd023eea90f8d0f20fbd537
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2019-05-26 22:20:57 +0530

    Update 18disparmstrong.py

--- 18disparmstrong.py
+++ 18disparmstrong.py
@@ -1,11 +1,11 @@
-start=int(input())
-end=int(input())
-for num in range(start,end+1):
+a=int(input())
+b=int(input())
+for num in range(a,b):
     sum=0
     temp=num
     while(temp>0):
        digit=temp%10
-       sum += digit**3
-       temp //= 10
-    if(num==sum):
+       sum=sum+digit**3
+       temp=temp//10
+    if(sum==num):
         print(num)
