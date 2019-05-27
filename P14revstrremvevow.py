18bdc470019a1725b28a5675ac1173f572f6026e
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2019-05-27 20:27:10 +0530

    Update P14revstrremvevow.py

--- P14revstrremvevow.py
+++ P14revstrremvevow.py
@@ -2,7 +2,7 @@ a=str(input())
 l=list(a)
 for i in range(0,len(a)):
     q=a[i].lower()
-    if(q=="a" or q=="e" or q=="i" or q=="o" or q=="u" or q=="A" or q=="E" or q=="I" or q=="O" or q=="U"):
+    if(q=="a" or q=="e" or q=="i" or q=="o" or q=="u"):
         l.remove(q)
 for i in reversed(l):
     print(i,end=" ")
