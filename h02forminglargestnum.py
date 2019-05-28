c5a411d05c15ce78d30484288bbd9e0e43668092
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2019-05-28 15:43:34 +0530

    Update h02forminglargestnum.py

--- h02forminglargestnum.py
+++ h02forminglargestnum.py
@@ -1,16 +1,8 @@
-def printMaximum(inum): 
-    count = [0 for x in range(10)] 
-    string = str(num) 
-    for i in range(len(string)): 
-        count[int(string[i])] = count[int(string[i])] +  1
-    result = 0
-    multiplier = 1
-    for i in range(10): 
-        while count[i] > 0: 
-            result = result + ( i * multiplier ) 
-            count[i] = count[i] - 1
-            multiplier = multiplier * 10
-    return result 
-n=int(input())
-num=int(input())
-print(printMaximum(num) )
+c=int(input())
+l=list(map(int,input().split()))
+l1=sorted(l)
+d=l1[::-1]
+rest=''
+for i in range(len(d)):
+    rest=rest+str(d[i])+''
+print(rest)
