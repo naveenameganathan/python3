5453d1da821b77b0c07dc8bafc5f6df55d7a3425
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2019-06-24 09:38:16 +0530

    Create P202vowels.py

---
+++ P202vowels.py
@@ -0,0 +1,7 @@
+p=input()
+for i in p:
+    if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
+        print(i,end="")
+for i in p:
+    if i!="a" and i!="e" and i!="i" and i!="o" and i!="u":
+        print(i,end="")
