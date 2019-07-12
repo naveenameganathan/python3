a99b986ad9062a882aa3d8a44da5c0e4eabd7700
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2019-07-12 19:46:22 +0530

    Create O20minnum.py

---
+++ O20minnum.py
@@ -0,0 +1,16 @@
+x6,y6=map(int,input().split())
+arr=list(map(int,input().split()))
+arr.sort(reverse=True)
+a=0
+total=y6
+for i in arr:
+    if total>=i:
+        rem=int(total/i)
+        a+=rem
+        total=total - (i*rem)
+    if total==0:
+        break
+if total==0:
+    print(a)
+else:
+    print("it's not possible to sum up coins in such a way that they um upto",y6) 
