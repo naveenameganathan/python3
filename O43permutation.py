d8778fe6120e46cc4dda234c9adfacae0f922a72
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2019-07-13 13:49:03 +0530

    Update O43permutation.py

--- O43permutation.py
+++ O43permutation.py
@@ -1,21 +1,21 @@
-p = int(input())
-q = list(map(int,input().split()))
-c,l = 0,[]
-b = [x for x in range(1,p+1)]
-for i in q:
-  if i in b:
-    b.remove(i)
-d = 0
-for i in range(p-1):
-  p = q[i]
-  for j in range(i+1,p):
-    if p == q[j]:
-      if p < b[d]:
-        q[j] = b[d]
-        c += 1
+v6=int(input())
+k=list(map(int,input().split()))
+s,f=0,[]
+d=[x for x in range(1,v6+1)]
+for i in k:
+  if i in d:
+    d.remove(i)
+m=0
+for i in range(0,v6-1):
+  h=k[i]
+  for j in range(i+1,v6):
+    if h==k[j]:
+      if h<d[m]:
+        k[j]=d[m]
+        s+=1
       else:
-        q[i] = b[d]
-        c += 1
-      d += 1
-print(c)
-print(*q)
+        k[i]=d[m]
+        s+=1
+      m+=1
+print(s)
+print(*k)
