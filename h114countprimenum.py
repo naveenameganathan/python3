6bff6f16de0a03dd36bedec140935c3af56b983f
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2019-07-25 23:06:39 +0530

    Create h114countprimenum.py

---
+++ h114countprimenum.py
@@ -0,0 +1,10 @@
+p,q=map(int,input().split())
+c=0
+for i in range(p,q + 1):
+   if  i> 1:
+       for j in range(2,i):
+           if (i%j) == 0:
+               break
+       else:
+           c+=1
+print(c)
