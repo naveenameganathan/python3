a1c5cb3521d052365a6bc77e2183b60e7b65423c
naveenameganathan <50620127+naveenameganathan@users.noreply.github.com>
2019-07-25 22:51:13 +0530

    Update P201catalannum.py

--- P201catalannum.py
+++ P201catalannum.py
@@ -1,14 +1,14 @@
 p=int(input())
-def binomialCoefficient(n, k): 
-    if (k > n - k): 
-        k = n - k 
-    res = 1
-    for i in range(k): 
-        res = res * (n - i) 
-        res = res // (i + 1) 
-    return res 
 def catalan(n): 
-    c = binomialCoefficient(2*n, n) 
-    return c//(n + 1) 
+    if (n == 0 or n == 1): 
+        return 1
+    catalan = [0 for i in range(n + 1)] 
+    catalan[0] = 1
+    catalan[1] = 1 
+    for i in range(2, n + 1): 
+        catalan[i] = 0
+        for j in range(i): 
+            catalan[i] = catalan[i] + catalan[j] * catalan[i-j-1] 
+    return catalan[n] 
 for i in range (p+1): 
     print (catalan(i),end=" ") 
